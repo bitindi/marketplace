04f3ff7953628c65abea7d8776fda017a79019cd
Kumar <119077822+kumarbitindi@users.noreply.github.com>
2023-03-22 08:34:36 -0600

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -2,8 +2,8 @@
 
 // 1. Set up the network your smart contracts are deployed to.
 // First, import the chain from the package, then set the NETWORK variable to the chain.
-import { Bitindi } from "@thirdweb-dev/chains";
-export const NETWORK = Bitindi;
+import { 4099 } from "@thirdweb-dev/chains";
+export const NETWORK = 4099;
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
